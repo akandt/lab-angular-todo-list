3656b68991c5b1bf2a9baed100d1122165cf054f
austin <austinkandt@gmail.com>
2020-09-23 17:41:29 -0400

    Commiting part 1 of lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
+import { TransitiveCompileNgModuleMetadata } from '@angular/compiler';
+import { i18nMetaToDocStmt } from '@angular/compiler/src/render3/view/i18n/meta';
 import { Component } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+interface Todo {
+  task: string,
+  completed: boolean
+}
 
 @Component({
-  selector: 'app-root',
+  selector: 'todo',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'to-do-list';
+
+  listItems: Todo[] = [
+    {
+      task: 'fold clothes',
+      completed: false
+    },
+    {
+      task: 'relax',
+      completed: false
+    },
+    {
+      task: 'try to pet cat',
+      completed: true
+    },
+    {
+      task: 'pet dog',
+      completed: false
+    },
+  ];
+
 }
